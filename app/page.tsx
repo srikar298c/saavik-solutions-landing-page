02b2b09bddd22cebf644ed8a7cd6ce279abddaad
kudurmalla srikar <srikar2985969c@gmail.com>
2025-07-31 11:45:45 +0530

    refactor(page): comment out EnterpriseSolutions component
    Temporarily disable EnterpriseSolutions section while redesigning the component to better align with new product offerings.

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       </div>
       <AboutUs />
       <Services />
-      <EnterpriseSolutions />
+      {/* <EnterpriseSolutions /> */}
   <Products />
       {/* <Testimonials /> */}
       <BlogSection />
